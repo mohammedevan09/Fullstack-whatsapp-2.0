eafdb624ed92dfb4cc98f104a74dd6f3ecc328a4
Mohammed Evan <121313913+mohammedevan09@users.noreply.github.com>
2023-09-06 15:05:07 +0600

    Update ChatList.jsx

--- client/src/components/Chatlist/ChatList.jsx
+++ client/src/components/Chatlist/ChatList.jsx
@@ -21,7 +21,7 @@ function ChatList() {
   return (
     <div
       className={`flex bg-[black] text-white flex-col max-h-screen z-20 w-screen lg:w-full md:visible ${
-        isOpenChatList ? 'visible' : 'hidden'
+        isOpenChatList ? 'visible' : 'responsive-three'
       }`}
     >
       {pageType === 'default' && (

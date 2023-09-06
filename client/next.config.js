7de30eb3956ffe09290619fd0f1dc375cb21d2b7
Mohammed Evan <121313913+mohammedevan09@users.noreply.github.com>
2023-09-06 14:22:31 +0600

    Update next.config.js

--- client/next.config.js
+++ client/next.config.js
@@ -6,6 +6,7 @@ const nextConfig = {
       'avatars.githubusercontent.com',
       'res.cloudinary.com',
       'localhost',
+      'whatsapp-clone-backend-xhjc.onrender.com',
       '',
     ],
   },
